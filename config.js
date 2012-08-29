ea33df94755819805db199e4e37e5c7a79f751d0
vflash <flash-vkv@rambler.ru>
2012-08-29 12:03:32 +0400

    up

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 ﻿
 exports.port = 1777;
 exports.host = '127.0.0.1';
-exports.auth_domains = '.vflash.ru .timtoi.vn';
+exports.auth_domains = '.vflash.ru';
 exports.log = true;
 exports.strict = false;
 
