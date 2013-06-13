626f8f96a141c35130cda2df4bca4a697d4952dc
vflash <flash-vkv@rambler.ru>
2013-06-13 14:33:38 +0400

    up

---
+++ config.js
@@ -0,0 +1,12 @@
+﻿var config = exports;
+
+config.port = 1778;
+config.host = '127.0.0.1';
+config.auth_domains = 'vflash.ru .vflash.ru .zzreader.com .timtoi.vn';
+config.gzip = true;
+config.log = true;
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,2 @@
+﻿
+require('./lib/server.js');
